8b762f7d54a90c5c2aa30fb11b8b57ea1413ab54
Bruno Moreira <brunomoreiramg@gmail.com>
2023-09-20 22:11:46 -0300

    Java Initial Files

---
+++ HoraDoSistema/HoraDoSistema.java
@@ -0,0 +1,9 @@
+package HoraDoSistema;
+import java.util.Date;
+
+public class HoraDoSistema {
+    public static void main(String[] args) {
+        Date DateHora = new Date();
+        System.out.println("A hora do sistema é: " + DateHora.toString()); 
+    }
+}
