002957310f07d65d82f9b5f576e6827cef3a3e0f
Bruno Moreira <brunomoreiramg@gmail.com>
2023-09-24 12:01:44 -0300

    Hora do sistema na pasta RelogioSwing

---
+++ RelogioSwing/swing/src/main/java/com/relogio/Main.java
@@ -0,0 +1,9 @@
+package RelogioSwing.swing.src.main.java.com.relogio;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do meu sistema é: " + relogio.toString());
+    }
+}
