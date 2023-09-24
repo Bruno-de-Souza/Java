21486a4014b02d873744e55d1eef7ab99fb03db7
Bruno Moreira <brunomoreiramg@gmail.com>
2023-09-24 20:34:57 -0300

    Atualizando a pasta TiposPrimitivos

--- TiposPrimitivos/TiposPrimitivos.java
+++ TiposPrimitivos/TiposPrimitivos.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class TiposPrimitivos {
     public static void main(String args[]) {
-        Scanner teclado = new Scanner(System.in);
-        System.out.print("Digite o nome do aluno: ");
-        String nome = teclado.nextLine();
-        System.out.print("Digite a nota do aluno: ");
-        float nota = teclado.nextFloat();
-        //System.out.print("A nota é: " + nota);
-        //System.out.println("A nota é: " + nota);
-        //System.out.printf("A nota do %s é %.4f \n", nome, nota);
-        System.out.format("A nota do %s é %.2f\n", nome, nota);
+        try (Scanner teclado = new Scanner(System.in)) {
+            System.out.print("Digite o nome do aluno: ");
+            String nome = teclado.nextLine();
+            System.out.print("Digite a nota do aluno: ");
+            float nota = teclado.nextFloat();
+            //System.out.print("A nota é: " + nota);
+            //System.out.println("A nota é: " + nota);
+            //System.out.printf("A nota do %s é %.4f \n", nome, nota);
+            System.out.format("A nota do %s é %.2f\n", nome, nota);
+        }
     }
 }
