eeab7f68d081fd44de4d26e83edc4913f25d5e12
Bruno Moreira <brunomoreiramg@gmail.com>
2023-09-24 22:15:20 -0300

    Pasta CalcularIdade

---
+++ CalculadoraIdade/CalculadoraIdade.java
@@ -0,0 +1,10 @@
+package CalculadoraIdade;
+
+public class CalculadoraIdade {
+    public static void main(String args[]) {
+        int anoNascimento = 1989;
+        int anoAtual = 2023;
+        int idade = anoAtual - anoNascimento;
+        System.out.println("A minha idade atual é: " + idade);
+    }
+}
