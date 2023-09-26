f6e2b403afd3c186782abe0034821f1f1ba46b55
Bruno Moreira <brunomoreiramg@gmail.com>
2023-09-25 23:24:28 -0300

    Pasta Operadores

---
+++ Operadores/Operadores.java
@@ -0,0 +1,30 @@
+package Operadores;
+import java.lang.Math;
+
+public class Operadores {
+    public static void main(String args[]) {
+        /*int n1 = 3;
+        int n2 = 5;
+        float media = (n1 + n2) / 2;
+        System.out.println("A média é igual a: " + media);*/
+
+        /*int numero = 5;
+        int valor = 10 + numero ++;
+        System.out.println(valor);
+        System.out.println(numero);*/
+
+        /*int x = 7;
+        x += 3;
+        System.out.println(x);*/
+
+        /*int x = 9;
+        System.out.println(Math.sqrt(x));*/
+
+        /*float valor = 8.3f;
+        int arrendondar = (int) Math.floor(valor);
+        System.out.println(arrendondar);*/
+
+        double valorAleatorio = Math.random();
+        System.out.println(valorAleatorio);
+    }
+}
