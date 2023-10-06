b749d5488e11dc04384daef0759eb407b2755003
Bruno Moreira <brunomoreiramg@gmail.com>
2023-10-05 23:16:33 -0300

    Pasta EstruturasCondicionais

---
+++ EstruturasCondicionais/EstruturaCompostas/EstruturasCondicionais.java
@@ -0,0 +1,19 @@
+package EstruturasCondicionais.EstruturaCompostas;
+
+import java.util.Scanner;
+
+public class EstruturasCondicionais {
+    public static void main(String arg []) {
+        try (Scanner teclado = new Scanner(System.in);) {
+            System.out.println("Digite a sua data de nascimento: ");
+            int nascimento = teclado.nextInt();
+            int idade = 2023 - nascimento;
+            System.out.println("Sua idade é: " + idade + " anos");
+            if( idade >= 18) {
+                System.out.println("Maior de idade!");
+            } else {
+                System.out.println("Menor de idade!");
+            }
+        }
+    }
+}
