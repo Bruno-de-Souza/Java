7f6da248d25262ae54701c1c38b2a30557aa4dcd
Bruno Moreira <brunomoreiramg@gmail.com>
2023-10-15 22:39:40 -0300

    Estruturas de Repetição

---
+++ EstruturaDeRepetição/EstruturaDeRepeticaoWhile.java
@@ -0,0 +1,11 @@
+package EstruturaDeRepetição;
+
+public class EstruturaDeRepeticaoWhile {
+    public static void main(String[] args) {
+        int cc = 0;
+        while (cc < 4) {
+            System.out.println("Cambalhota " + cc);
+            cc++;
+        }
+    }
+}
