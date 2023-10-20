2cd84d2f89af0a4027f7bd084120182fb0f1585e
Bruno Moreira <brunomoreiramg@gmail.com>
2023-10-19 23:22:35 -0300

    Estrutura de repetição - Continue, Break. Contador

--- EstruturaDeRepetição/EstruturaDeRepeticaoWhile.java
+++ EstruturaDeRepetição/EstruturaDeRepeticaoWhile.java
@@ -3,9 +3,15 @@ package EstruturaDeRepetição;
 public class EstruturaDeRepeticaoWhile {
     public static void main(String[] args) {
         int cc = 0;
-        while (cc < 4) {
-            System.out.println("Cambalhota " + cc);
+        while (cc < 10) {
             cc++;
+            if(cc == 1 || cc == 3 || cc == 5) {
+                continue;
+            }
+            if(cc == 7) {
+                break;
+            }
+            System.out.println("Cambalhota " + cc);
         }
     }
 }
