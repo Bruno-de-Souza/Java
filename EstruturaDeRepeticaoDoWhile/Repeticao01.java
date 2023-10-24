d92dc41669d7a36297d8bc08858b153752bc3835
Bruno Moreira <brunomoreiramg@gmail.com>
2023-10-23 22:24:38 -0300

    Estrtura de repetição "Do While"

---
+++ EstruturaDeRepeticaoDoWhile/Repeticao01.java
@@ -0,0 +1,11 @@
+package EstruturaDeRepeticaoDoWhile;
+
+public class Repeticao01 {
+    public static void main(String[] args) {
+        int cc = 0;
+        do {
+            System.out.println(cc);
+            cc++;
+        } while (cc < 4);
+    }
+}
