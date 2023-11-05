c92ded67d82c1caee9c4950fcf8db433634346ce
Bruno Moreira <brunomoreiramg@gmail.com>
2023-11-05 10:42:47 -0300

    Estrutura de repetição For. For aninhado "I" "J".

---
+++ EstruturaRepeticaoFor/Cambalhota.java
@@ -0,0 +1,11 @@
+/*
+package EstruturaRepeticaoFor;
+
+public class Cambalhota {
+    public static void main(String [] args) {
+        for (int cc = 10; cc <= 100; cc+=10) {
+            System.out.println(cc);
+        }
+    }
+} 
+*/
