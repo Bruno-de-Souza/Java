c7c77b60f2e040720d587386756fa15cb6e00536
Bruno Moreira <brunomoreiramg@gmail.com>
2023-11-12 15:59:40 -0300

    Aula de Metódos e Funções

---
+++ Funções/Funcoes.java
@@ -0,0 +1,12 @@
+public class Funcoes {
+    static int soma(int a, int b) {
+        int s = a + b;
+        return s;
+    }
+    public static void main(String[] args) {
+        System.out.println("Começou o programa no ***** main *****");
+        int sm = soma(10, 25);
+        System.out.println("A soma é: " + sm);
+    }
+}
+    
---
+++ Metódos/Procedimento.java
@@ -0,0 +1,12 @@
+/*package Metódos;
+
+public class Procedimento {
+    static void soma(int a, int b) {
+        int s = a + b;
+        System.out.println("A soma é: " + s);
+    }
+    public static void main(String[] args) {
+        System.out.println("Começou o programa no ***** main *****");
+        soma(10, 25);
+    }
+}*/
